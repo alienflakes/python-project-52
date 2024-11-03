ef158c6edfcc914c1da6cbe760aec73fadb4163c
alienflakes <alienflakes38@gmail.com>
2024-11-03 20:38:45 +0300

    fix: make index view class-based

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 
-def index(request):
-    return render(request, 'index.html')
+class IndexView(TemplateView):
+
+    def get(self, request):
+        return render(request, 'index.html')
