ce45efa1d7f20b1466f695b56e56208c70b545e2
alienflakes <alienflakes38@gmail.com>
2024-11-04 01:57:36 +0300

    feat: add login/logout

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -144,3 +144,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = 'index'
--- task_manager/urls.py
+++ task_manager/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from task_manager import views
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('', views.IndexView.as_view()),
+    path('', TemplateView.as_view(template_name="index.html"), name="index"),
+    path('', include('django.contrib.auth.urls')),
     path('users/', include('users.urls')),
     path('admin/', admin.site.urls)
 ]
