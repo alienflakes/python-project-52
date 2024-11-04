8399267790f6d0aa63c8b4a99453f2e14a779415
alienflakes <alienflakes38@gmail.com>
2024-11-04 19:06:47 +0300

    feat: basic signup

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 
 urlpatterns = [
-    path('', views.UsersView.as_view(), name='users_view')
+    path('', views.UsersView.as_view(), name='users_view'),
+    path('create/', views.SignUpView.as_view(), name='signup'),
+    # path('<int:pk>/update/'),
+    # path('<int:pk>/delete/')
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import View, ListView
-from django.contrib.auth import models
+from django.views.generic import ListView, CreateView
+from django.contrib.auth import models, forms
+from django.urls import reverse_lazy
 
 
 class UsersView(ListView):
     queryset = models.User.objects.all()
     template_name = 'users.html'
+
+
+class SignUpView(CreateView):
+    form_class = forms.UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
