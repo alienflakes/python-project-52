d3f675695a69c1363da9979a11ea745cdce5e820
alienflakes <alienflakes38@gmail.com>
2024-11-08 20:42:59 +0300

    fix: change 'id' to 'pk' in urls for auto usage in views

--- users/urls.py
+++ users/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.UsersView.as_view(), name='users'),
     path('create/', views.SignUpView.as_view(), name='signup'),
-    path('<int:id>/update/', views.CustomUpdateView.as_view(), name='update'),
-    path('<int:id>/delete/', views.DeleteView.as_view(), name='delete')
+    path('<int:pk>/update/', views.CustomUpdateView.as_view(), name='update'),
+    path('<int:pk>/delete/', views.DeleteView.as_view(), name='delete')
 ]
