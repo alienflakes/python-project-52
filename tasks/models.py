a6c080e2763715e9d6148e76019d52d71a1fbbe9
alienflakes <alienflakes38@gmail.com>
2024-11-12 23:27:16 +0300

    feat: add Task-related i18n

--- tasks/models.py
+++ tasks/models.py
@@ -1,17 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
 from statuses.models import Status
+from django.utils.translation import gettext_lazy as _
 
 
 class Task(models.Model):
 
-    name = models.CharField(max_length=150)
-    description = models.TextField(default='', null=True, blank=True)
-    status = models.ForeignKey(Status, on_delete=models.PROTECT)
+    name = models.CharField(max_length=150, verbose_name=_('Name'))
+    description = models.TextField(default='', null=True, blank=True,
+                                   verbose_name=_('Description'))
+    status = models.ForeignKey(Status, on_delete=models.PROTECT,
+                               verbose_name=_('Status'))
     creator = models.ForeignKey(User, on_delete=models.PROTECT,
-                                related_name='creator')
+                                related_name='creator',
+                                verbose_name=_('Creator'))
     doer = models.ForeignKey(User, on_delete=models.PROTECT,
-                             related_name='doer', null=True, blank=True)
+                             null=True, blank=True,
+                             related_name='doer',
+                             verbose_name=_('Doer'))
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
