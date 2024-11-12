575fcf3dff7b8e45d5978a77bdad105ee83b975a
alienflakes <alienflakes38@gmail.com>
2024-11-12 23:11:19 +0300

    fix: add __str__ with model's name for better displaying

--- statuses/models.py
+++ statuses/models.py
@@ -5,3 +5,6 @@ from django.utils.translation import gettext_lazy as _
 class Status(models.Model):
     name = models.CharField(max_length=100, verbose_name=_('Name'))
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
