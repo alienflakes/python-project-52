8c323694b6884f79eb7e8eb8ab907106b747b99c
alienflakes <alienflakes38@gmail.com>
2024-11-20 23:45:04 +0300

    feat: add first user permission tests

--- users/tests.py
+++ users/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
+# from tasks.models import Task
+# from statuses.models import Status
 
 
 USERNAME = 'vince69'
@@ -59,3 +61,35 @@ class UserCRUDCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertRedirects(response, '/users/')
         self.assertFalse(User.objects.filter(username=USERNAME).exists())
+
+
+class UserPermissionsCase(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        user1 = User.objects.create(username=USERNAME)
+        user1.set_password(PASSWORD)
+        user1.save()
+
+        user2_id = 2
+        user2 = User.objects.create(username=USERNAME2,
+                                    id=user2_id)
+        user2.set_password(PASSWORD2)
+        user2.save()
+
+    def login(self):
+        self.client.login(username=USERNAME, password=PASSWORD)
+        return User.objects.get(username=USERNAME)
+
+    def test_logged_out_access(self):
+        response = self.client.get('/tasks/', follow=True)
+        self.assertRedirects(response, '/login/?next=/tasks/')
+
+    def test_update_other_user(self):
+        self.login()
+        user2_id = User.objects.get(username=USERNAME2).id
+        response = self.client.get(f'/users/{user2_id}/update/', follow=True)
+        self.assertRedirects(response, '/users/')
+
+    # def test_delete_other_task(self):
+    # definetely need fixtures for this (users, status, task)
