9a99e8c6c3d8630d8099915f868e76b7991dadaf
alienflakes <alienflakes38@gmail.com>
2024-11-21 17:31:29 +0300

    feat: add test for deleting other's task, some refactoring

--- users/tests.py
+++ users/tests.py
@@ -1,95 +1,84 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-# from tasks.models import Task
-# from statuses.models import Status
+from tasks.models import Task
+from statuses.models import Status
 
 
-USERNAME = 'vince69'
-NEW_USERNAME = 'JELLY_FOX'
-FIRST_NAME = 'Noel'
-LAST_NAME = 'Fielding'
-PASSWORD = 'pinkcrow'
-
-USERNAME2 = '2theMoon'
-PASSWORD2 = 'ihatejazz'
+def login_user1(self):
+    user = User.objects.get(id=1)
+    self.client.force_login(user)
+    return user
 
 
 class UserCRUDCase(TestCase):
 
-    @classmethod
-    def setUpTestData(cls):
-        user = User.objects.create(username=USERNAME)
-        user.set_password(PASSWORD)
-        user.save()
-
-    def login(self):
-        self.client.login(username=USERNAME, password=PASSWORD)
-        return User.objects.get(username=USERNAME)
+    fixtures = ['user1.json']
 
     def test_user_create(self):
+        username = '2theMoon'
+        password = 'ihatejazz'
         response = self.client.post('/users/create/', {
-            'username': USERNAME2,
-            'password1': PASSWORD2,
-            'password2': PASSWORD2
+            'username': username,
+            'password1': password,
+            'password2': password
         }, follow=True)
         self.assertEqual(response.status_code, 200)
         self.assertRedirects(response, '/login/')
-        self.assertTrue(User.objects.filter(username=USERNAME2).exists())
+        self.assertTrue(User.objects.filter(username=username).exists())
 
     def test_user_update(self):
-        user = self.login()
+        user = login_user1(self)
+        new_username = 'JELLY_FOX'
+        first_name = 'Noel'
+        last_name = 'Fielding'
+        password = 'pinkcrow'
 
         response = self.client.post(f'/users/{user.id}/update/', {
-            'username': NEW_USERNAME,
-            'first_name': FIRST_NAME,
-            'last_name': LAST_NAME,
-            'password1': PASSWORD,
-            'password2': PASSWORD
+            'username': new_username,
+            'first_name': first_name,
+            'last_name': last_name,
+            'password1': password,
+            'password2': password
         }, follow=True)
         self.assertEqual(response.status_code, 200)
         self.assertRedirects(response, '/users/')
 
-        user = User.objects.get(username=NEW_USERNAME)
-        self.assertEqual(user.first_name, FIRST_NAME)
-        self.assertEqual(user.last_name, LAST_NAME)
+        user = User.objects.get(username=new_username)
+        self.assertEqual(user.first_name, first_name)
+        self.assertEqual(user.last_name, last_name)
 
     def test_user_delete(self):
-        user = self.login()
+        user = login_user1(self)
 
         response = self.client.post(f'/users/{user.id}/delete/',
                                     follow=True)
         self.assertEqual(response.status_code, 200)
         self.assertRedirects(response, '/users/')
-        self.assertFalse(User.objects.filter(username=USERNAME).exists())
+        self.assertFalse(User.objects.filter(id=user.id).exists())
 
 
 class UserPermissionsCase(TestCase):
 
-    @classmethod
-    def setUpTestData(cls):
-        user1 = User.objects.create(username=USERNAME)
-        user1.set_password(PASSWORD)
-        user1.save()
-
-        user2_id = 2
-        user2 = User.objects.create(username=USERNAME2,
-                                    id=user2_id)
-        user2.set_password(PASSWORD2)
-        user2.save()
-
-    def login(self):
-        self.client.login(username=USERNAME, password=PASSWORD)
-        return User.objects.get(username=USERNAME)
+    fixtures = ['user1.json', 'user2.json']
 
     def test_logged_out_access(self):
+        # accessing other pages (/statuses, /labels) would get the same result
+        # because they import this access mixin from tasks/views.py
         response = self.client.get('/tasks/', follow=True)
         self.assertRedirects(response, '/login/?next=/tasks/')
 
     def test_update_other_user(self):
-        self.login()
-        user2_id = User.objects.get(username=USERNAME2).id
-        response = self.client.get(f'/users/{user2_id}/update/', follow=True)
+        login_user1(self)
+        response = self.client.get('/users/2/update/', follow=True)
         self.assertRedirects(response, '/users/')
 
-    # def test_delete_other_task(self):
-    # definetely need fixtures for this (users, status, task)
+    def test_delete_other_task(self):
+        task = Task.objects.create(
+            name='task1',
+            status=Status.objects.create(name='test_status'),
+            creator=User.objects.get(id=2)
+        )
+        login_user1(self)
+        response = self.client.get(f'/tasks/{task.id}/delete/')
+        self.assertRedirects(response, '/tasks/')
+        self.assertTrue(Task.objects.filter(id=1).exists())
